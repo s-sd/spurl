a7f79ac246fb05ac67df5f79080f53598bdf284d
Shaheer U Saeed <shaheersd@gmail.com>
2023-06-29 11:47:07 +0100

    Issue #2: add algorithm argument to core functions

--- spurl/core.py
+++ spurl/core.py
@@ -2,10 +2,10 @@ import tensorflow as tf
 import gymnasium as gym
 import numpy as np
 
-def train(env, policy_network, trials, episodes_per_trial, epcohs_per_trial, batch_size):
+def train(env, policy_network, algorithm, trials, episodes_per_trial, epcohs_per_trial, batch_size):
     return None
 
-def test(env, policy_network, trials, episodes_per_trial):
+def test(env, policy_network, algorithm, trials, episodes_per_trial):
     return None
     
 
