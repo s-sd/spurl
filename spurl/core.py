188104fcf0b61896c31c225b667fc5cead597810
Shaheer U Saeed <shaheersd@gmail.com>
2023-06-29 11:54:16 +0100

    Issue #5: add additional args to core functions

--- spurl/core.py
+++ spurl/core.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import gymnasium as gym
 import numpy as np
 
-def train(env, policy_network, algorithm, trials, episodes_per_trial, epcohs_per_trial, batch_size):
+def train(env, policy_network, algorithm, trials, episodes_per_trial, epcohs_per_trial, batch_size, save_frequency, save_path, save_name, verbose):
     return None
 
 def test(env, policy_network, algorithm, trials, episodes_per_trial):
