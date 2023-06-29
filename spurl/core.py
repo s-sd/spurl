21a579ad54cc8a7b0100c4a48d8b7dcb00ed69f2
Shaheer U Saeed <shaheersd@gmail.com>
2023-06-29 11:42:02 +0100

    Issue #2: refactor code, add dummy demo, and create core and utils

---
+++ spurl/core.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import gymnasium as gym
+import numpy as np
+
+def train(env, policy_network, trials, episodes_per_trial, epcohs_per_trial, batch_size):
+    return None
+
+def test(env, policy_network, trials, episodes_per_trial):
+    return None
+    
+
