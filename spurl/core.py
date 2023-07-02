baf0e213fbef9e40828021bedff1b8f0aeedd5f3
Shaheer U Saeed <shaheersd@gmail.com>
2023-07-02 11:47:59 +0100

    Issue #5: add test entrypoint

--- spurl/core.py
+++ spurl/core.py
@@ -10,10 +10,13 @@ def train(algorithm, trials, episodes_per_trial, epochs_per_trial, batch_size, v
     return algorithm
 
 def test(algorithm, trials, episodes_per_trial):
+    mean_rewards = []
     for trial in range(trials):
         print(f'Test Trial: {trial+1}/{trials}')
         states, actions, rewards = algorithm.run(episodes_per_trial)
-        print(f'    Mean Reward: {np.mean(rewards)}')
-    return None
+        mean_reward = np.mean(rewards)
+        mean_rewards.append(mean_reward)
+        print(f'    Mean Reward: {mean_reward}')
+    return mean_rewards
     
 
