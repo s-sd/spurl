a62ad67e9aff61744afee38c957df1ddaa0b1094
Shaheer U Saeed <shaheersd@gmail.com>
2023-08-10 13:10:45 +0100

    Issue #17: add explicit noise to discrete actions

--- demos/cartpole_single_agent.py
+++ demos/cartpole_single_agent.py
@@ -17,7 +17,7 @@ def build_policy_network(state_shape, num_actions):
     dropout1 = tf.keras.layers.Dropout(0.4)(dense1)
     dense2 = tf.keras.layers.Dense(32, activation='relu')(dropout1)
     dropout2 = tf.keras.layers.Dropout(0.4)(dense2)
-    dense3 = tf.keras.layers.Dense(14, activation='relu')(dropout2)    
+    dense3 = tf.keras.layers.Dense(14, activation='relu')(dropout2)     
     dense4 = tf.keras.layers.Dense(num_actions, activation='softmax')(dense3)
     policy_network = tf.keras.Model(inputs=inputs, outputs=dense4)
     return policy_network
@@ -29,14 +29,14 @@ num_actions = env.action_space.n
 
 policy_network = build_policy_network(state_shape, num_actions)
 
-reinforce = REINFORCE(env, policy_network, artificial_truncation=512)
+reinforce = REINFORCE(env, policy_network, artificial_truncation=512, noise_scale=0.1)
 
 # change some parameters of the algorithm
 reinforce.gamma = 0.99
 reinforce.learning_rate = 0.0001
 reinforce.optimizer = tf.keras.optimizers.Adam(reinforce.learning_rate, epsilon=1e-6, clipnorm=1e1)
 
-reinforce = train(reinforce, trials=64, episodes_per_trial=8, epochs_per_trial=4, batch_size=32, verbose=True)
+reinforce = train(reinforce, trials=32, episodes_per_trial=8, epochs_per_trial=2, batch_size=16, verbose=True)
 
 rewards, lengths = test(reinforce, trials=2, episodes_per_trial=8, deterministic=True)
 
--- demos/pendulum_single_agent.py
+++ demos/pendulum_single_agent.py
@@ -22,7 +22,7 @@ def build_policy_network(state_shape, action_size):
     
     scaled_outputs = tf.keras.layers.Lambda(lambda x: (x + 1) * 2 - 2)(dense4) # scale to action space
     
-    policy_network = tf.keras.Model(inputs=inputs, outputs=dense4)
+    policy_network = tf.keras.Model(inputs=inputs, outputs=scaled_outputs)
     return policy_network
 
 env = gym.make("Pendulum-v1")
@@ -48,7 +48,7 @@ if not os.path.exists(temp_path):
 
 for meta_trial in range(meta_trials):
     print(f'\nMeta Trial: {meta_trial+1} / {meta_trials}\n')
-    reinforce = train(reinforce, trials=2, episodes_per_trial=8, epochs_per_trial=2, batch_size=16, verbose=True)    
+    reinforce = train(reinforce, trials=2, episodes_per_trial=8, epochs_per_trial=8, batch_size=32, verbose=True)    
     rewards, lengths = test(reinforce, trials=1, episodes_per_trial=4, deterministic=True)
     
     # linearly annealling scale as time goes on
