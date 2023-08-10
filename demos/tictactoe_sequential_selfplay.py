Issue #9: fix bugs in step computation

demos/tictactoe_sequential_selfplay.py
@@ -6,6 +6,7 @@ import tensorflow as tf
 import gymnasium as gym
 import numpy as np
 import os
+import shutil
 
 from envs.tictactoe import TicTacToeEnv
 
@@ -55,7 +56,6 @@ for meta_trial in range(meta_trials):
     print(f'\nMeta Trial: {meta_trial+1} / {meta_trials}\n')
     reinforce = train(reinforce, trials=2, episodes_per_trial=32, epochs_per_trial=2, batch_size=32, verbose=True)    
     rewards, lengths = test(reinforce, trials=1, episodes_per_trial=4, deterministic=True)
-    print(lengths)
     if lengths > 6.0:
         break
     
@@ -66,7 +66,10 @@ for meta_trial in range(meta_trials):
 # =============================================================================
 
 opponents_path = r'./temp/tictactoe_ops'
-if not os.path.exists(opponents_path):
+if os.path.exists(opponents_path):
+    shutil.rmtree(opponents_path)
+    os.mkdir(opponents_path)
+else:
     os.mkdir(opponents_path)
 
 reinforce = REINFORCE_TicTacToe(env, policy_network, artificial_truncation=256, self_play_type='fictitious', opponent_save_frequency=8, opponents_path=opponents_path, noise_scale=0.2)
@@ -79,10 +82,30 @@ for meta_trial in range(meta_trials):
     print(f'\nMeta Trial: {meta_trial+1} / {meta_trials}\n')
     reinforce = train(reinforce, trials=2, episodes_per_trial=16, epochs_per_trial=2, batch_size=32, verbose=True)    
     rewards, lengths = test(reinforce, trials=1, episodes_per_trial=4, deterministic=True)
-    print(lengths)
     if lengths > 6.0:
         break
 
 # =============================================================================
 # Prioritised Fictitious Self-Play
 # =============================================================================
+
+opponents_path = r'./temp/tictactoe_ops'
+if os.path.exists(opponents_path):
+    shutil.rmtree(opponents_path)
+    os.mkdir(opponents_path)
+else:
+    os.mkdir(opponents_path)
+
+reinforce = REINFORCE_TicTacToe(env, policy_network, artificial_truncation=256, self_play_type='prioritised', opponent_save_frequency=8, opponents_path=opponents_path, noise_scale=0.2)
+
+reinforce.optimizer = tf.keras.optimizers.Adam(reinforce.learning_rate, epsilon=1e-6, clipnorm=1e1)
+
+meta_trials = 1024
+
+for meta_trial in range(meta_trials):
+    print(f'\nMeta Trial: {meta_trial+1} / {meta_trials}\n')
+    reinforce = train(reinforce, trials=2, episodes_per_trial=16, epochs_per_trial=2, batch_size=32, verbose=True)    
+    rewards, lengths = test(reinforce, trials=1, episodes_per_trial=4, deterministic=True)
+    if lengths > 6.0:
+        break
+

spurl/algorithms/reinforce/base.py
@@ -53,8 +53,11 @@ class REINFORCE:
             episode_rewards = []
             episode_actions = []
             episode_states = []
+            
+            step_number = 0
                         
             while True:
+                
                 action = self.select_action(state, deterministic)
                 
                 reshaped_action = np.reshape(np.squeeze(np.array(action, dtype=np.uint32)), self.env.action_space.shape)
@@ -71,9 +74,11 @@ class REINFORCE:
                 
                 state = next_state
                 
+                step_number += 1
+                
                 #terminate episode at artificial truncation number of steps
                 if self.artificial_truncation is not None:
-                    if episode_number > self.artificial_truncation:
+                    if step_number > self.artificial_truncation:
                         done = True
                 
                 if done:

spurl/algorithms/reinforce/self_play_sequential_discrete.py
@@ -65,7 +65,6 @@ class REINFORCE(discrete.REINFORCE):
                 if len(opponents_list) > 1.0:
                     selected_opponent = self.opponent_sampler(opponents_list)
                     policy_network = tf.keras.models.load_model(os.path.join(self.opponents_path, opponents_list[int(selected_opponent)]))
-                    print('selected from bank')
                 else:
                     policy_network = self.policy_network
                                 
@@ -108,6 +107,8 @@ class REINFORCE(discrete.REINFORCE):
             if episode_number % self.opponent_save_frequency == 0 and self.self_play_type != 'vanilla':
                 self.policy_network.save(os.path.join(self.opponents_path, str(num_opponents+1)))
                 
+            step_number = 0
+            
             while True:
                 
                 if self.env.current_player_num == 1:
@@ -135,9 +136,11 @@ class REINFORCE(discrete.REINFORCE):
                 
                 state = next_state
                 
+                step_number += 1
+                
                 #terminate episode at artificial truncation number of steps
                 if self.artificial_truncation is not None:
-                    if episode_number > self.artificial_truncation:
+                    if step_number > self.artificial_truncation:
                         done = True
                 
                 if done: